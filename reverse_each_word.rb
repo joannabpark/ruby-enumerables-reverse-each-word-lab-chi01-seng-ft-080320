c86aa877b845164a4e3cf4b95dbb098393ce3b70
JoAnna Park <joannabpark1@gmail.com>
2020-07-23 04:15:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,4 @@
 def reverse_each_word(string)
   original_array = string.split("")  
   new_array = []
-  original_array.collect do {|string| string.reverse}
-  new_array << 
-end
-  end
-  new_array.join("")
-end
+  original_array.collect do |string|
