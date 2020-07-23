8c1768213dd195da1aa41f6dad443215d72d3b3c
JoAnna Park <joannabpark1@gmail.com>
2020-07-23 04:18:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
   original_array = string.split(" ")  
   new_array = []
-original_array.collect do {|string| new_array << string.reverse}
+original_array.collect do {|string| string.reverse}
+new_array << string.reverse
   end
   new_array.join(" ")
 end
